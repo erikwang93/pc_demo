a9e073e3e70a4877c62b899b9a1065dac24291c5
erikwang93 <erikwang93@gmail.com>
2017-08-15 22:14:53 +0800

    Initial commit

---
+++ test.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver =  webdriver.Safari()
+driver.get('http://www.lairmey.com/login')
+
+elem = driver.find_element_by_name('userName')
+elem.send_keys('admin')
+elem = driver.find_element_by_name('password')
+elem.send_keys('Farewell2014')
+
+elem = driver.find_element_by_name('commit')
+elem.click()
